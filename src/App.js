f00b08b3cc799ee47f354b9416f682546b7e2372
David Azdejkovic <noxdavid@elfak.rs>
2022-03-10 03:25:53 +0100

    (createPlayer in prepare method)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Piramida from './Components/Piramida/Piramida';
 import Containter_AQ from './Components/Containter_AQ/Container_AQ';
 import useSound from 'use-sound';
 import play from "/kviz_final/kviz_app/src/assets/play2.mp3";
-
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Start_Game from './Components/Start_Game/Start_Game';
 
 function App() 
 {
@@ -15,7 +16,7 @@ function App()
   const [questionNumber,setQuestionNumber] = useState(1);
   const [stop,setStop] = useState(false);
   const [timer, setTimer] = useState(30);
-
+  const [start,setStart] = useState(true);
   const restart = () =>
   {
     setTimer(30);
@@ -35,24 +36,34 @@ function App()
 
 
   return (
-    <div className='app'>
-    <div className='main'>
-  
-<div className='top'> 
+     
     
-    <Timer  setStop={setStop} questionNumber={questionNumber} stop={stop} restart={restart}/>
-    </div>
-    <div className='bottom'>
-      
-    <Containter_AQ questionNumber={questionNumber} setQuestionNumber={setQuestionNumber} setStop={setStop} stop={stop} />
-      </div>
-    </div>
+    <>
+  {start ? (<div className='app'>
 
-    <Piramida questionNumber={questionNumber}/>
+<div className='main'>
+
+<div className='top'> 
+
+<Timer  setStop={setStop} questionNumber={questionNumber} stop={stop} restart={restart}/>
+</div>
+<div className='bottom'>
+  
+<Containter_AQ questionNumber={questionNumber} setQuestionNumber={setQuestionNumber} setStop={setStop} stop={stop} />
+  </div>
+</div>
+
+<Piramida questionNumber={questionNumber}/>
+
+
+</div> ) : <Start_Game/>  
     
-   
-  </div> 
-  );
+  
 }
+  </> )}
+ 
+  
+  
+
 
 export default App;
---
+++ src/Components/Start_Game/Start_Game.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+
+export default function Start_Game() {
+  
+    
+    return (
+      <>
+    <div className='start'>Start_Game</div>
+  
+    </>
+  )
+
+
+}
